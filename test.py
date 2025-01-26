478338c3afc2abbe1dc1119f6127371ee66828e9
ghome <guo_yuao@sjtu.edu.cn>
2025-01-26 12:07:02 +0800

    lab

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ rgb = np.array(Image.open("testdata/006600-color.png").convert('RGB'))
 depth = np.array(cv2.imread("testdata/006600-depth.png", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH))
 true = np.array(cv2.imread("testdata/006600-depth_true.png", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH))
 ppl = SwinDRNetPipeline(model_path)
-result = ppl.inference(rgb, depth, 4)
+result = ppl.inference(rgb, depth, 4, 1500)
 
 # show
 rgb_scaled = cv2.normalize(rgb, None, 0, 255, cv2.NORM_MINMAX)
